77ad8fe1d654aa190ffa664d5ddabf650bd3bea0
dgbailey <bailey.dust@gmail.com>
2020-04-27 20:39:25 -0700

    deploy

--- knexfile.js
+++ knexfile.js
@@ -2,33 +2,28 @@
 require('dotenv').config();
 
 module.exports = {
-	// development: {
-	// 	client: 'pg',
-	// 	connection: {
-	// 		host: '127.0.0.1',
-	// 		database: process.env.DB_LOCAL_DEV_NAME,
-	// 		user: process.env.DB_LOCAL_UN,
-	// 		password: process.env.DB_LOCAL_PW
-	// 	},
-	// 	pool: {
-	// 		min: 2,
-	// 		max: 10
-	// 	},
-	// 	migrations: {
-	// 		directory: './data/migrations'
-	// 	},
-	// 	seeds: {
-	// 		directory: './data/seeds'
-	// 	}
-	// },
-	production: {
+	development: {
 		client: 'pg',
 		connection: {
-			host: process.env.HOST,
-			database: process.env.DATABASE,
-			user: process.env.USER,
-			password: process.env.PASSWORD
+			host: '127.0.0.1',
+			database: process.env.DB_LOCAL_DEV_NAME,
+			user: process.env.DB_LOCAL_UN,
+			password: process.env.DB_LOCAL_PW
 		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			directory: './data/migrations'
+		},
+		seeds: {
+			directory: './data/seeds'
+		}
+	},
+	production: {
+		client: 'pg',
+		connection: process.env.DATABASE_URL,
 
 		pool: {
 			min: 2,
