f626dc88a30d6bcc67bfb9913993aa203230b181
dgbailey <bailey.dust@gmail.com>
2020-04-27 20:22:24 -0700

    heroku postgres config variables

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,12 @@ module.exports = {
 	},
 	production: {
 		client: 'pg',
-		connection: process.env.DATABASE_URL,
+		connection: {
+			host: process.env.HOST,
+			database: process.env.DATABASE,
+			user: process.env.USER,
+			password: process.env.PASSWORD
+		},
 
 		pool: {
 			min: 2,
