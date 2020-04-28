06ae105acfe5efb92952d382a5fa36342d1a81e3
dgbailey <bailey.dust@gmail.com>
2020-04-27 19:29:20 -0700

    config for hosted postgres db

--- knexfile.js
+++ knexfile.js
@@ -2,26 +2,39 @@
 require('dotenv').config();
 
 module.exports = {
-
-  
-  development: {
-    client: 'pg',
-    connection: {
-      host: '127.0.0.1',
-      database: process.env.DB_LOCAL_DEV_NAME,
-      user:process.env.DB_LOCAL_UN,
-      password: process.env.DB_LOCAL_PW
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: './data/migrations'
-    },
-    seeds: {
-      directory: './data/seeds'
-    }
-  },
-
+	development: {
+		client: 'pg',
+		connection: {
+			host: '127.0.0.1',
+			database: process.env.DB_LOCAL_DEV_NAME,
+			user: process.env.DB_LOCAL_UN,
+			password: process.env.DB_LOCAL_PW
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			directory: './data/migrations'
+		},
+		seeds: {
+			directory: './data/seeds'
+		}
+	},
+	production: {
+		client: 'pg',
+		connection: {
+			database: process.env.DATABASE_URL
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			directory: './data/migrations'
+		},
+		seeds: {
+			directory: './data/seeds'
+		}
+	}
 };
