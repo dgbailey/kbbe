c9ce3bc994f70d4c63870c725f6cc30123a4544d
dgbailey <bailey.dust@gmail.com>
2020-04-27 19:38:20 -0700

    connection url updated for postgres

--- knexfile.js
+++ knexfile.js
@@ -23,9 +23,9 @@ module.exports = {
 	},
 	production: {
 		client: 'pg',
-		connection: {
-			database: process.env.DATABASE_URL
-		},
+		connection: process.env.DATABASE_URL,
+		host: process.env.DATABASE_URL,
+
 		pool: {
 			min: 2,
 			max: 10
